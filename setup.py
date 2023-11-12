95d8de0549165231764a692b7637989e1bcb4dd7
zmfkzj <qlwlal@naver.com>
2023-11-13 00:00:38 +0900

    setup.py 수정 중

--- setup.py
+++ setup.py
@@ -2,4 +2,4 @@ from setuptools import setup, find_packages
 
 setup(name="youtube_crawler",
       version='0.0.1',
-      packages=find_packages(where='src', exclude=['test']))
+      packages=find_packages(where='src', exclude=['tests']))
