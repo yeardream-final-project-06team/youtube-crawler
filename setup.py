2c8f5f4c98e17fa842dc0ea90ae99437f3659277
zmfkzj <qlwlal@naver.com>
2023-11-13 00:06:12 +0900

    setup.py, Dockerfile 완성

--- setup.py
+++ setup.py
@@ -2,4 +2,5 @@ from setuptools import setup, find_packages
 
 setup(name="youtube_crawler",
       version='0.0.1',
-      packages=find_packages(where='src'))
+      packages=find_packages(where='src'),
+      package_dir={'':'src'})
